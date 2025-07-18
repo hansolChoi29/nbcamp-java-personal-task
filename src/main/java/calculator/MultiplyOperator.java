af43632a9e63bdd3e5032671c08599fc44d34ec5
Robbie <robbie331@>
2025-07-18 10:15:54 +0900

    chore: multiply에 대한 코드 설명 주석으로 추가

--- src/main/java/calculator/MultiplyOperator.java
+++ src/main/java/calculator/MultiplyOperator.java
@@ -1,12 +1,14 @@
 package calculator;
-
+// 곱셈 기능을 담당하는 클래스
+// Operator 인터페이스를 구현하여 '*' 연산을 처리함
 public class MultiplyOperator implements Operator{
 
+    // 연산 기호를 반환하는 메서드: '*' 리턴
     @Override
     public char getSymbol() {
         return '*';
     }
-
+    // 두 수를 곱한 결과를 반환하는 메서드 (a * b)
     @Override
     public double operator(double a, double b) {
         return a*b;
