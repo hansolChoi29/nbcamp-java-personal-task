dc87456350775c2a168681fd8d0cb27160197bc3
Robbie <robbie331@>
2025-07-18 10:14:29 +0900

    chore: subtract에 대한 코드 설명 주석으로 추가

--- src/main/java/calculator/SubtractOperator.java
+++ src/main/java/calculator/SubtractOperator.java
@@ -1,13 +1,15 @@
 package calculator;
 
+// 뺄셈 기능을 담당하는 클래스
+// Operator 인터페이스를 구현하여 '-' 연산을 처리함
 public class SubtractOperator implements Operator{
 
-
+    // 연산 기호를 반환하는 메서드: '-' 리턴
     @Override
     public char getSymbol() {
         return'-';
     }
-
+    // 두 수를 뺀 결과를 반환하는 메서드 (a - b)
     @Override
     public double operator(double a, double b) {
         return  a-b;
