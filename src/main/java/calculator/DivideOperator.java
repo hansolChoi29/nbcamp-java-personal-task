ee8c10d27c35d5b11d7ddc3f12701929728a4243
Robbie <robbie331@>
2025-07-18 10:16:51 +0900

    chore: divide 코드에 대한 설명 주석으로 추가

--- src/main/java/calculator/DivideOperator.java
+++ src/main/java/calculator/DivideOperator.java
@@ -1,13 +1,16 @@
 package calculator;
-
+// 나눗셈 기능을 담당하는 클래스
+// Operator 인터페이스를 구현하여 '/' 연산을 처리함
 public class DivideOperator implements Operator{
 
-
+    // 연산 기호를 반환하는 메서드: '/' 리턴
     @Override
     public char getSymbol() {
         return '/';
     }
 
+    // 두 수를 나눈 결과를 반환하는 메서드 (a / b)
+    // 단, b가 0이면 ArithmeticException 예외 발생
     @Override
     public double operator(double a, double b) {
         if(b==0){
